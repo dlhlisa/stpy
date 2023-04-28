61e5c1db9f5abd1503ffa04533e67bb93c0b380a
Lihua <denglihualisa@163.com>
2023-04-28 18:17:46 +0200

    Update trials.sql

--- tools_tips/trials.sql
+++ tools_tips/trials.sql
@@ -1,6 +1,5 @@
 -- t1: table name of your data group A, use NTILE(4) as an example 
--- t2: intermediate temp data table
--- t3: temp data table with calculated y_mean, x thresholds
+-- t3: temp data table with calculated y_mean, x thresholds, save the results as t3
 -- t4: table name of your data group B
 
 WITH t2 AS (SELECT t1_id, x, NTILE(4) OVER (ORDER BY x) 
@@ -14,6 +13,7 @@ WITH t2 AS (SELECT t1_id, x, NTILE(4) OVER (ORDER BY x)
 
 -- based on the result from above say table3 (t3), you know the x_max1-4
 -- assign the quntile group for table 4 (your group B data)
+
 SELECT x, y
 (CASE WHEN x <= x_max1 THEN 1
    WHEN x > x_max1 and x <= x_max2 THEN 2
@@ -25,3 +25,16 @@ from t4;
 select t4.y, t4.quantile, t3.y_mean,
 (case when y>y_mean THEN 1 ELSE 0 END) as select_label
 left join t3 on t3.quantile=t4.quantile; 
+
+
+-- #########################################################
+WITH t5 AS (SELECT x, y
+(CASE WHEN x <= x_max1 THEN 1
+   WHEN x > x_max1 and x <= x_max2 THEN 2
+   WHEN x > x_max2 and x <= x_max3 THEN 3
+   WHEN x > x_max3 and x <= x_max4 THEN 4
+   ELSE 5 END) AS quantile
+FROM t4) 
+SELECT t5.y, t5.quantile, t3.y_mean,
+(case when y>y_mean THEN 1 ELSE 0 END) as select_label from t3,t5
+where t3.quantile=t5.quantile; 
