e6d407eaaf24d90435c7648ea6e021b64cc7b981
Lihua1990 <dlhlisa@126.com>
2026-01-22 13:20:24 +0100

    add config.py

---
+++ stpy/config.py
@@ -0,0 +1,15 @@
+"""
+This contains some configurations.
+
+"""
+import numpy as np
+import pandas as pd
+
+import os
+import sys
+from pathlib import Path
+
+from pathlib import Path 
+PROJECT_ROOT = Path(__file__).resolve().parent
+sys.path.append(str(PROJECT_ROOT))
+# print(PROJECT_ROOT)
