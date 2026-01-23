397711d5d98aac8e1cb9af9836c8ac1a95207653
Lihua1990 <dlhlisa@126.com>
2026-01-23 15:04:53 +0100

    format by black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'stpy'
-copyright = '2026, Lihua Deng'
-author = 'Lihua Deng'
-release = '0.1.0'
+project = "stpy"
+copyright = "2026, Lihua Deng"
+author = "Lihua Deng"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -23,13 +23,12 @@ extensions = [
 
 autosummary_generate = True
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
